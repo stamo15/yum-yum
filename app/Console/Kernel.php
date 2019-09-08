9c8b5f6b24cb0b1725b3a3425ce0507678239a51
Salmane Tamo <stamo15@alustudent.com>
2019-09-08 23:14:59 +0400

    Created new trait RegisterUserTrait to create users

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('make:trait')->hourly();
         // $schedule->command('inspire')
         //          ->hourly();
     }
